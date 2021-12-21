c68a268a3ebd73e4b0dd9a18ab0d4972fa81d6d3
taem1n1 <youman7953@naver.com>
2021-12-21 19:51:09 +0900

    Initial Commit

---
+++ src/test/java/com/server/vari/VariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.vari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
