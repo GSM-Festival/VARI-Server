742ac867f0c6422fe501763ba86c8ce444e3ad3a
taem1n1 <youman7953@naver.com>
2021-12-22 12:02:03 +0900

    회원 Entity Modelling

--- src/main/java/com/server/vari/model/member/Member.java
+++ src/main/java/com/server/vari/model/member/Member.java
@@ -1,4 +1,29 @@
 package com.server.vari.model.member;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.*;
+
+@Entity @Table(name = "MEMBER")
+@Getter @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class Member {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "member_name", nullable = false)
+    private String name;
+
+    @Column(name = "member_email", nullable = false)
+    private String email;
+
+    @Column(name = "member_password", nullable = false)
+    private String password;
 }
