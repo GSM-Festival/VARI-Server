9bd563de9049fc6af1788945597d1000ab766f63
taem1n1 <youman7953@naver.com>
2021-12-22 12:01:46 +0900

    게시글 Entity Modelling

--- src/main/java/com/server/vari/model/board/Board.java
+++ src/main/java/com/server/vari/model/board/Board.java
@@ -1,4 +1,32 @@
 package com.server.vari.model.board;
 
+import com.server.vari.model.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+import static javax.persistence.GenerationType.*;
+
+@Entity @Table(name = "BOARD")
+@Getter @Builder
+@NoArgsConstructor @AllArgsConstructor
 public class Board {
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "board_title", nullable = false)
+    private String title;
+
+    @Column(name = "board_content", nullable = false)
+    private String content;
 }
