a50e922b27dedae32182f87586fb40322da1b152
taem1n1 <youman7953@naver.com>
2021-12-31 16:11:33 +0900

    Login 용 DTO 작성

--- src/main/java/com/server/vari/model/member/dto/MemberLoginDto.java
+++ src/main/java/com/server/vari/model/member/dto/MemberLoginDto.java
@@ -1,2 +1,16 @@
-package com.server.vari.model.member.dto;public class MemberLoginDto {
+package com.server.vari.model.member.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter @Setter @Builder
+@AllArgsConstructor @NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberLoginDto {
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
 }
