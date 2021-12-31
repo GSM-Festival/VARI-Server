cb3e15881ad3a3a57f1e7039279df611ca11ccd9
taem1n1 <youman7953@naver.com>
2021-12-31 16:12:17 +0900

    MyUserDetails 추가

--- src/main/java/com/server/vari/jwt/MyUserDetails.java
+++ src/main/java/com/server/vari/jwt/MyUserDetails.java
@@ -1,2 +1,21 @@
-package com.server.vari.jwt;public class MyUserDetails {
+package com.server.vari.jwt;
+
+import com.server.vari.model.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyUserDetails implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByName(username) // String 타입의 member primary key를 long타입으로 변환
+                .orElseThrow( () -> new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다."));
+    }
 }
