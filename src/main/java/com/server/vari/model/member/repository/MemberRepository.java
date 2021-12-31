120656ac45ab4cb7f2c60c65d0e7a3f9bd7c58fa
taem1n1 <youman7953@naver.com>
2021-12-31 16:13:42 +0900

    findByName 쿼리 추가

--- src/main/java/com/server/vari/model/member/repository/MemberRepository.java
+++ src/main/java/com/server/vari/model/member/repository/MemberRepository.java
@@ -3,5 +3,10 @@ package com.server.vari.model.member.repository;
 import com.server.vari.model.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByEmail(String email);
+    Optional<Member> findByName(String name);
 }
