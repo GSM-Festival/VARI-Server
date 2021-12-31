2101e3b81e7a2a5a136cd2405ad36b590167e728
taem1n1 <youman7953@naver.com>
2021-12-31 16:12:02 +0900

    UserDetails에 의한 Method 추가

--- src/main/java/com/server/vari/model/member/Member.java
+++ src/main/java/com/server/vari/model/member/Member.java
@@ -1,18 +1,28 @@
 package com.server.vari.model.member;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.server.vari.jwt.MyUserDetails;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static javax.persistence.GenerationType.*;
 
 @Entity @Table(name = "MEMBER")
 @Getter @Builder
 @NoArgsConstructor @AllArgsConstructor
-public class Member {
+public class Member implements UserDetails {
 
     @Id @GeneratedValue(strategy = IDENTITY)
     @Column(name = "member_id")
@@ -26,4 +36,53 @@ public class Member {
 
     @Column(name = "member_password", nullable = false)
     private String password;
+
+    @Column(name = "member_refreshToken")
+    private String refreshToken;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles
+                .stream().map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public String getUsername() {
+        return this.name;
+    }
+
+    @Override// 계정이 만료가 안됐는지
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override// 계정이 잠기지 않았는지
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override //계쩡 패스워드가 만료가 안됐는지
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override //계정이 사용 가능한지
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void updateRefreshToken(String refreshToken){
+        this.refreshToken = refreshToken;
+    }
+
 }
