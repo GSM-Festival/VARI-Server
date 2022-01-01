f047345c3a6f4a957b6c9aa7346dc4309b0d9a58
taem1n1 <youman7953@naver.com>
2022-01-01 23:06:09 +0900

    로그인한 멤버의 정보를 가져오는 CurrentUserUtil 생성

---
+++ src/main/java/com/server/vari/util/CurrentUserUtil.java
@@ -0,0 +1,28 @@
+package com.server.vari.util;
+
+import com.server.vari.model.member.Member;
+import com.server.vari.model.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CurrentUserUtil {
+
+    private final MemberRepository memberRepository;
+
+    public Member getCurrentUser() {
+
+        String username = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else{
+            username = principal.toString();
+        }
+        return memberRepository.findByName(username);
+    }
+
+}
