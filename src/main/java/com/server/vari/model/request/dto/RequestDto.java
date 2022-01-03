1ff1ada5368f8ce12588e564cb53a95027d818d0
taem1n1 <youman7953@naver.com>
2022-01-03 20:57:51 +0900

    Request form dto작성

--- src/main/java/com/server/vari/model/request/dto/RequestDto.java
+++ src/main/java/com/server/vari/model/request/dto/RequestDto.java
@@ -1,2 +1,33 @@
-package com.server.vari.model.request.dto;public class RequestDto {
+package com.server.vari.model.request.dto;
+
+import com.server.vari.model.board.Board;
+import com.server.vari.model.request.Request;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter @Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class RequestDto {
+
+    @NotBlank
+    private String stdNum;
+
+    @NotBlank
+    private String language;
+
+    @NotBlank
+    private String content;
+
+    public Request toEntity(String stdNum, String language, String content, Board board) {
+        return Request.builder()
+                .stdNum(stdNum)
+                .language(language)
+                .content(content)
+                .board(board)
+                .build();
+    }
+
 }
