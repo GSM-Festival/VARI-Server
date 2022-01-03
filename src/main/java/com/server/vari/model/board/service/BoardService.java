6959060f72663b23485803adb4c15138b6908246
taem1n1 <youman7953@naver.com>
2022-01-03 20:54:19 +0900

    게시판 서비스로직 구현

--- src/main/java/com/server/vari/model/board/service/BoardService.java
+++ src/main/java/com/server/vari/model/board/service/BoardService.java
@@ -1,2 +1,18 @@
-package com.server.vari.model.board.service;public interface BoardService {
+package com.server.vari.model.board.service;
+
+import com.server.vari.model.board.Board;
+import com.server.vari.model.board.dto.BoardContestDto;
+import com.server.vari.model.board.dto.BoardPortfolioDto;
+import com.server.vari.model.board.dto.BoardProjectDto;
+import com.server.vari.model.board.dto.BoardStudyDto;
+
+import java.util.List;
+
+public interface BoardService {
+
+    Board createBoardForPortfolio(BoardPortfolioDto boardPortfolioDto);
+    Board createBoardForProject(BoardProjectDto boardProjectDto);
+    Board createBoardForStudy(BoardStudyDto boardStudyDto);
+    Board createBoardForContest(BoardContestDto boardContestDto);
+    List<Board> findAllBoard();
 }
--- src/main/java/com/server/vari/model/board/service/BoardServiceImpl.java
+++ src/main/java/com/server/vari/model/board/service/BoardServiceImpl.java
@@ -1,2 +1,59 @@
-package com.server.vari.model.board.service;public class BoardServiceImpl {
+package com.server.vari.model.board.service;
+
+import com.server.vari.model.board.Board;
+import com.server.vari.model.board.dto.BoardContestDto;
+import com.server.vari.model.board.dto.BoardPortfolioDto;
+import com.server.vari.model.board.dto.BoardProjectDto;
+import com.server.vari.model.board.dto.BoardStudyDto;
+import com.server.vari.model.board.repository.BoardRepository;
+import com.server.vari.model.member.Member;
+import com.server.vari.util.CurrentUserUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BoardServiceImpl implements BoardService{
+
+    private final BoardRepository boardRepository;
+    private final CurrentUserUtil currentUserUtil;
+
+    @Override
+    public Board createBoardForPortfolio(BoardPortfolioDto boardPortfolioDto) {
+        Member currentUser = currentUserUtil.getCurrentUser();
+
+        Board board = boardRepository.save(boardPortfolioDto.toEntity(boardPortfolioDto.getTitle(), boardPortfolioDto.getContent(), currentUser));
+        return board;
+    }
+
+    @Override
+    public Board createBoardForProject(BoardProjectDto boardProjectDto) {
+        Member currentUser = currentUserUtil.getCurrentUser();
+
+        Board board = boardRepository.save(boardProjectDto.toEntity(boardProjectDto.getTitle(), boardProjectDto.getContent(), currentUser));
+        return board;
+    }
+
+    @Override
+    public Board createBoardForStudy(BoardStudyDto boardStudyDto) {
+        Member currentUser = currentUserUtil.getCurrentUser();
+
+        Board board = boardRepository.save(boardStudyDto.toEntity(boardStudyDto.getTitle(), boardStudyDto.getContent(), currentUser));
+        return board;
+    }
+
+    @Override
+    public Board createBoardForContest(BoardContestDto boardContestDto) {
+        Member currentUser = currentUserUtil.getCurrentUser();
+
+        Board board = boardRepository.save(boardContestDto.toEntity(boardContestDto.getTitle(), boardContestDto.getContent(), currentUser));
+        return board;
+    }
+
+    @Override
+    public List<Board> findAllBoard() {
+        return boardRepository.findAll();
+    }
 }
