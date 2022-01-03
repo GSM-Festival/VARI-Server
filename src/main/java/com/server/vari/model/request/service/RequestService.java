32a72ee0e2b7e259ea3f7d7d0c4c00cafebfdc6c
taem1n1 <youman7953@naver.com>
2022-01-03 20:56:18 +0900

    신청서 서비스로직스 구현

--- src/main/java/com/server/vari/model/request/service/RequestService.java
+++ src/main/java/com/server/vari/model/request/service/RequestService.java
@@ -1,2 +1,10 @@
-package com.server.vari.model.request.service;public class RequestService {
+package com.server.vari.model.request.service;
+
+import com.server.vari.model.request.Request;
+import com.server.vari.model.request.dto.RequestDto;
+
+public interface RequestService {
+
+    Request createRequest(RequestDto requestDto, Long id);
+
 }
--- src/main/java/com/server/vari/model/request/service/RequestServiceImpl.java
+++ src/main/java/com/server/vari/model/request/service/RequestServiceImpl.java
@@ -1,2 +1,25 @@
-package com.server.vari.model.request.service;public class RequestServiceImpl {
+package com.server.vari.model.request.service;
+
+import com.server.vari.model.board.Board;
+import com.server.vari.model.board.repository.BoardRepository;
+import com.server.vari.model.request.Request;
+import com.server.vari.model.request.dto.RequestDto;
+import com.server.vari.model.request.repository.RequestRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RequestServiceImpl implements RequestService{
+
+    private final BoardRepository boardRepository;
+    private final RequestRepository requestRepository;
+
+    @Override
+    public Request createRequest(RequestDto requestDto, Long id) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
+
+        return requestRepository.save(requestDto.toEntity(requestDto.getStdNum(), requestDto.getLanguage(), requestDto.getContent(), board));
+    }
 }
